ae4095ffbb55c857f7eb7643251ea9adb70ba02a
dylanchristopherson <dchristropherson@gmail.com>
2018-07-06 14:22:38 +0000

    Added a temp test

---
+++ tempF/temp.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float=temperatureF();
+
+  Serial.println("The temperature is: ");
+  Serial.println(float);
+}
