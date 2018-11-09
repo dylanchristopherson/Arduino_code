7dbe5a42a30fec777062b8ac5ddc485b90935a95
dylanchristopherson <dchristropherson@gmail.com>
2018-11-08 22:28:14 -0600

    Updated hc12 file to include link to help

--- hc12/hc12.ino
+++ hc12/hc12.ino
@@ -1,7 +1,11 @@
 /*    Arduino Long Range Wireless Communication using HC-12
                       Example 01
    by Dejan Nedelkovski, www.HowToMechatronics.com
+
+   https://howtomechatronics.com/tutorials/arduino/arduino-and-hc-12-long-range-wireless-communication-module/
 */
+
+
 #include <SoftwareSerial.h>
 SoftwareSerial HC12(10, 11); // HC-12 TX Pin, HC-12 RX Pin
 void setup() {
